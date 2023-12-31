c6aaa4829710efd701f2bb8e823ecef5e4207025
Shivam Shinde <shivamshindenewasa@gmail.com>
2023-12-31 13:03:47 +0530

    my first commit

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println(a);
+
+        System.out.println("Hello java");
+    }
+}
